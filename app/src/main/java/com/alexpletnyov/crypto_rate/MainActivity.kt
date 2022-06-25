ab2a9f37e27404fbd4dee6a6cdec46d87bbaaad3
Alexander Pletnyov <pletnevlive@gmail.com>
2022-06-26 00:21:20 +0900

    feat: add receiving data from the internet

--- app/src/main/java/com/alexpletnyov/crypto_rate/MainActivity.kt
+++ app/src/main/java/com/alexpletnyov/crypto_rate/MainActivity.kt
@@ -2,10 +2,34 @@ package com.alexpletnyov.crypto_rate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.alexpletnyov.crypto_rate.api.ApiFactory
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+	private val compositeDisposable = CompositeDisposable()
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+		val disposable = ApiFactory.apiService.getFullPriceList(
+			fromSymbols = "BTC,ETH,EOS"
+		)
+			.subscribeOn(Schedulers.io())
+			.observeOn(AndroidSchedulers.mainThread())
+			.subscribe({
+				Log.d("TEST_OF_LOADING_DATA", "Success: $it")
+			}, {
+				Log.d("TEST_OF_LOADING_DATA", "Error: $it")
+			})
+		compositeDisposable.add(disposable)
+	}
+
+	override fun onDestroy() {
+		super.onDestroy()
+		compositeDisposable.dispose()
 	}
 }
